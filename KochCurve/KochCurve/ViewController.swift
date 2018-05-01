3e0b1e5817886492e253a5ea5bdd9d39217546b2
L <L862608263@163.com>
2018-05-01 21:32:44 +0800

    create_project

---
+++ KochCurve/KochCurve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KochCurve
+//
+//  Created by L on 2018/5/1.
+//  Copyright © 2018年 L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
